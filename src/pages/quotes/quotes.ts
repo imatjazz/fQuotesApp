c258991e105d50d36e1e569f3d5894f40b155a27
Tim Do <tiendq83@gmail.com>
2018-01-31 07:43:19 +1100

    implement OnInit

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
 
 //@IonicPage()
 @Component({
   selector: 'page-quotes',
   templateUrl: 'quotes.html',
 })
-export class QuotesPage {
+export class QuotesPage implements OnInit {
+  quoteGroup: {category: string, quotes: Quote[], icon: string};
 
   constructor(
     public navCtrl: NavController, 
-    private navParams: NavParams) {
-  }
+    private navParams: NavParams) {};
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotesPage');
+  ngOnInit(){
+    this.quoteGroup = this.navParams.data;
   }
+  //ionViewDidLoad() {
+  //  console.log('ionViewDidLoad QuotesPage');
+  // this.quoteGroup = this.navParams.data;
+  // Add Elvis operator (?) in the template to use this approach
+  //}
 
 }
