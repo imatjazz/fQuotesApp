f8971d7cb64ab2a6c6f1d95bb1115425684f4bfe
AU\tiendo <tiendq83@gmail.com>
2018-01-30 18:50:12 +1100

    modify staging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
-
+//this is staging
 @NgModule({
   declarations: [
     MyApp,
